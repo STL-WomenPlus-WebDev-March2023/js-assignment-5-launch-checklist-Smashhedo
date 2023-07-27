16852d99d4bf1df83b40e4f630e139df3f95b734
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-07-27 09:59:38 -0500

    Moved lines 8-20 to be after 31 in script.js

--- script.js
+++ script.js
@@ -5,7 +5,17 @@
 //const { pickPlanet } = require("./scriptHelper");
 
 window.addEventListener("load", function() {
-    let listedPlanets;
+        let form = document.querySelector("form");
+          form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            const pilotName = document.getElementsByName('pilotName')[0]
+            const copilotName = document.getElementsByName('copilotName')[0]
+            const fuelLevelStatus = document.getElementsByName('fuelLevel')[0]
+            const cargoMass = document.getElementsByName('cargoMass')[0]
+            const launchStatus = document.getElementById('launchStatus')
+            const list = document.getElementById('faultyItems');
+            formSubmission(document, list, pilotName, copilotName, fuelLevelStatus, cargoMass);
+            let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
@@ -18,17 +28,6 @@ window.addEventListener("load", function() {
     
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
-
-        let form = document.querySelector("form");
-          form.addEventListener("submit", function(event) {
-            event.preventDefault();
-            const pilotName = document.getElementsByName('pilotName')[0]
-            const copilotName = document.getElementsByName('copilotName')[0]
-            const fuelLevelStatus = document.getElementsByName('fuelLevel')[0]
-            const cargoMass = document.getElementsByName('cargoMass')[0]
-            const launchStatus = document.getElementById('launchStatus')
-            const list = document.getElementById('faultyItems');
-            formSubmission(document, list, pilotName, copilotName, fuelLevelStatus, cargoMass);
                  }) 
           });
 
